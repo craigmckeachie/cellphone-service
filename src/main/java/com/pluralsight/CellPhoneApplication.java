bbd069a5dec51de2c8f1151e45fac2f032479838
Craig McKeachie <craigdmckeachie@gmail.com>
2026-04-14 14:49:23 -0400

    Cellphone class working

---
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class CellPhoneApplication {
+
+    public static void main(String[] args) {
+        CellPhone cellPhone = new CellPhone();
+        cellPhone.setOwner("Craig McKeachie");
+        System.out.println(cellPhone.getOwner());
+
+
+    }
+}
